af06d4a5b16550107af60ed0922bc2accc4e96b2
vocaltech <it@vocaltech.fr>
2022-02-13 12:46:14 +0100

    update

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-auth-passportjs'),
+      dir: require('path').join(__dirname, './coverage/ng-auth-passportjs-jwt'),
       subdir: '.',
       reporters: [
         { type: 'html' },
