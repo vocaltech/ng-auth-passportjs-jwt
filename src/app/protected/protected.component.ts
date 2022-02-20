6a3db2d10c20e70d72828e311f118b85906c1e59
vocaltech <it@vocaltech.fr>
2022-02-20 20:37:22 +0100

    update constructor and ngOnInit

--- src/app/protected/protected.component.ts
+++ src/app/protected/protected.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { environment } from 'src/environments/environment';
 
+import { AuthService } from '../services/auth.service'
+
 @Component({
   selector: 'app-protected',
   templateUrl: './protected.component.html',
@@ -9,25 +11,42 @@ import { environment } from 'src/environments/environment';
 })
 
 export class ProtectedComponent implements OnInit {
-  message!: string;
+  unauthorizedMessage = '';
+  isLoggedIn = false;
+  username = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService
+  ) {}
 
   ngOnInit(): void {
+    this.isLoggedIn = this.auth.isLoggedIn()
+
+    if (this.isLoggedIn) {
+      this.username = this.auth.getUsername();
+      console.log(`logged true - access granted for ${this.username}`)
+
+    } else {
+      console.log('logged false - check access from server!')
+      this.checkAccessFromServer()
+    }
+  }
+
+  private checkAccessFromServer = () => {
     this.http.get(environment.url_protected).subscribe(
       (response: any) => {
         console.log(response)
-        this.message = response.message
+        this.unauthorizedMessage = response.message
+        this.isLoggedIn = true
       }, 
       (error: any) => {
         console.log(error)
-        error.status === 401 ? this.message = error.statusText: this.message = error.message
+        error.status === 401 ? this.unauthorizedMessage = error.statusText: this.unauthorizedMessage = error.message
       },
       () => {
         console.log('done')
       }
     )
-
-
   }
 }
