90afcb2467c3de4df8f97a08b92e919ee43b5aa6
vocaltech <it@vocaltech.fr>
2022-02-20 20:39:39 +0100

    implement logout and some getters/setters

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,9 +6,25 @@ import { Injectable } from '@angular/core';
 
 //TODO: expiresAt w/date-fns
 export class AuthService {
+  private _isLoggedIn = false;
+  private _username = '';
+
   constructor() { }
+
+  isLoggedIn = () => this._isLoggedIn 
+  setLoggedIn = (val: boolean) => this._isLoggedIn = val
+
+  getUsername = () => this._username;
+  setUsername = (val: string) => this._username = val
 
   setLocalStorage(responseObj: any) {
     localStorage.setItem('id_token', responseObj.token)
+  }
+
+  logout() {
+    this._isLoggedIn = false;
+    this._username = '';
+    
+    localStorage.removeItem('id_token');
   }
 }
