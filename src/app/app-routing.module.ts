26a35345a45407686a32b47f24705f037db99f7f
vocaltech <it@vocaltech.fr>
2022-02-25 23:04:04 +0100

    add dashboard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { ProtectedComponent } from './protected/protected.component';
 import { LogoutComponent } from './logout/logout.component'
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   //{ path: '', redirectTo: "login", pathMatch: 'full'},
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
   { path: "protected", component: ProtectedComponent },
+  { path: "dashboard", component: DashboardComponent},
   { path: "logout", component: LogoutComponent }
 ];
 
